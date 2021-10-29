61c08dad87be24550324e130aa597e2833a73da5
Quinn Cooperstein <quinn.flynn.coop@gmail.com>
2021-10-29 15:56:45 -0500

    First working version with turboman Iframe!!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { Turboman } from './turboman';
 
 function App() {
   var Home = "<h1 class='text-5xl text-white m-3'>Welcome to Alloew!</h1><br /><h4 class='text-lg text-white'>A website where me and my friends make things for fun!</h4>";
@@ -10,8 +9,7 @@ function App() {
     "<br /><h4 class='text-lg text-white'>Jacob : Map Designer</h4>" +
     "<br />";
   
-  var Other = "<a" +
-    "href='https://discord.com/api/oauth2/authorize?client_id=887830738391662592&amp;permissions=0&amp;scope=bot' class='btn-tertiary' target='_blank' rel='noopener noreferrer'>Invite Donkey Bot to your server! (Click me)</a>";
+  var Other = "<a href='https://discord.com/api/oauth2/authorize?client_id=887830738391662592&amp;permissions=0&amp;scope=bot' class='btn-tertiary' target='_blank' rel='noopener noreferrer'>Invite Donkey Bot to your server! (Click me)</a>";
   
   var SimpsonSells = "<div class='w-full h-full bg-white rounded-lg'>" +
     "<p style='text-align: center;'>" +
@@ -31,13 +29,12 @@ function App() {
     "<p style='text-align: center;'><br></p> <p style='text-align: center;'><br><a class='pure-button' href='#' download='https://i.ibb.co/CJRvbVJ/Simpson3-8.png' width='100' height='25'>Download</a></p>" +
     "<p style='text-align: center;'><br><style>img {border: 2px solid #555;}</style><img class='m-auto' src='https://i.ibb.co/5FWsvqW/Simpson4-5.png' width='auto' height='720'></p> <p style='text-align: center;'><br></p>" +
     "<p style='text-align: center;'><br><a class='pure-button' href='#' download='https://i.ibb.co/5FWsvqW/Simpson4-5.png';' width='100' height='25'>Download</button></p></div>";
-
+  
+  var tb = "<h1 class='text-white text-center text-5xl'>TurboMan!</h1>" +
+    "<br /><iframe class='h-screen' src='../B1.1.1/index.html'></iframe><br /> <B111 />";
+  
   function iframe(id, content) {
     document.getElementById(id).innerHTML = content;
-  };
-
-  function B111() {
-    document.getElementById('section').innerHTML = "<h1 class='text-5xl'>B1.1.1</h1>";
   };
 
   return (
@@ -48,7 +45,7 @@ function App() {
           onClick={() => iframe('section', Home)}
         >Home</button>
         <button class="btn-primary"
-          onClick={() => iframe('section', "")}
+          onClick={() => iframe('section', tb)}
         >TurboMan</button>
         <button class="btn-primary"
           onClick={() => iframe('section', Credits)}
